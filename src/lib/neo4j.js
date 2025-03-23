f4671b0749e794ada17c8b6c2575934c50afaaeb
root <root@DESKTOP-TC9A0SV>
2025-03-22 20:35:25 -0500

    Fixed dotenv import in neo4j

--- src/lib/neo4j.js
+++ src/lib/neo4j.js
@@ -1,9 +1,12 @@
+import 'dotenv/config';
 import neo4j from 'neo4j-driver';
 
-const URI = process.env.NEO4J_URI || 'bolt://localhost:7687';
-const USER = process.env.NEO4J_USER || 'neo4j';
-const PASSWORD = process.env.NEO4J_PASSWORD || 'dj897wryfsac!#';
+const URI = process.env.NEO4J_URI;
+const USER = process.env.NEO4J_USER;
+const PASSWORD = process.env.NEO4J_PASSWORD;
 
-const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD), {
+    encrypted: 'ENCRYPTION_OFF'
+});
 
 export default driver;
